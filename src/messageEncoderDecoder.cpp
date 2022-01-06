83c9ebecd445c6360a7796836e6b03bd651ea4e8
Niv <nivh@securingsam.com>
2022-01-06 12:10:44 +0200

    Changed the encoding method, to support function encoders for each message type, using an appendable array

--- src/messageEncoderDecoder.cpp
+++ src/messageEncoderDecoder.cpp
@@ -6,11 +6,18 @@
 #include <ctime>
 #include <sstream>
 #include <iomanip>
+#include <algorithm>
 //#include <messageEncoderDecoder.h>
 #include "../include/messageEncoderDecoder.h"
 
-
 using std::string;
+
+struct encoderCallback {
+    std::string actionName;
+    int (*callback)(const std::string &, char *);
+};
+
+
 
 void shortToBytes(short num, char *bytesArr) {
     bytesArr[0] = ((num>>8))&0xFF;
@@ -32,76 +39,132 @@ int appendToBytes(char *bytesArr, std::string str) {
     return index;
 }
 
+int encodeRegister(const std::string &input, char *bytes) {
+    int length = 2;
+    shortToBytes(1, bytes);
+    int next_delimiter = 0;
+    for (int i = 0; i < 3; i++) {
+        length += appendToBytes(&bytes[length],
+                                input.substr(next_delimiter, input.find(' ', next_delimiter) - next_delimiter));
+        next_delimiter = input.find(' ', next_delimiter) + 1;
+        bytes[length++] = 0;
+    }
+    return length;
+}
+
+int encodePM(const std::string &input, char *bytes) {
+    int length = 2;
+
+    shortToBytes(6, bytes);
+    int next_delimiter = 0;
+    for (int i = 0; i < 2; i++) {
+        length += appendToBytes(&bytes[length],
+                                input.substr(next_delimiter, input.find(' ', next_delimiter) - next_delimiter));
+        next_delimiter = input.find(' ', next_delimiter) + 1;
+        bytes[length++] = 0;
+    }
+    std::time_t timer = std::time(0);
+    std::tm *now = std::localtime(&timer);
+    std::stringstream time_str_stream;
+    time_str_stream << std::put_time(now, "%d-%m-%Y %H-%M");
+    length += appendToBytes(&bytes[length], time_str_stream.str());
+    bytes[length++] = 0;
+
+    return length;
+}
+
+int encodeLogin(const std::string &input, char *bytes) {
+    int length = 2;
+
+    shortToBytes(2, bytes);
+    int next_delimiter = 0;
+    for (int i = 0; i < 2; i++) {
+        length += appendToBytes(&bytes[length],
+                                input.substr(next_delimiter, input.find(' ', next_delimiter) - next_delimiter));
+        next_delimiter = input.find(' ', next_delimiter) + 1;
+        bytes[length++] = 0;
+    }
+    bytes[length++] = static_cast<char>(stoi(input.substr(next_delimiter)));
+
+    return length;
+}
+
+int encodeLogout(const std::string &input, char *bytes) {
+    shortToBytes(3, bytes);
+    return 2;
+}
+
+int encodeFollow(const std::string &input, char *bytes) {
+    int length = 2;
+    size_t next_delimiter;
+    shortToBytes(4, bytes);
+    bytes[length++] = static_cast<char>(stoi(input, &next_delimiter));
+    next_delimiter += 2;
+    length += appendToBytes(&bytes[length], input.substr(next_delimiter));
+    bytes[length++] = 0;
+
+    return length;
+}
+
+int encodePost(const std::string &input, char *bytes) {
+    int length = 2;
+
+    shortToBytes(5, bytes);
+    length += appendToBytes(&bytes[length],input);
+    bytes[length++] = 0;
+
+    return length;
+}
+
+int encodeLogstat(const std::string &input, char *bytes) {
+    shortToBytes(7, bytes);
+    return 2;
+}
+
+int encodeStat(const std::string &input, char *bytes) {
+    int length = 2;
+
+    shortToBytes(8, bytes);
+    std::string input_copy(input);
+    std::replace(input_copy.begin(), input_copy.end(), ' ', '|');
+    length += appendToBytes(&bytes[length], input_copy);
+    bytes[length++] = 0;
+
+    return length;
+}
+
+int encodeBlock(const std::string &input, char *bytes) {
+    int length = 2;
+
+    shortToBytes(12, bytes);
+    length += appendToBytes(&bytes[length], input);
+    bytes[length++] = 0;
+
+    return length;
+}
+
+static const struct encoderCallback handlers[] = {  {.actionName = "REGISTER", .callback=encodeRegister},
+                                                    {.actionName = "LOGIN", .callback=encodeLogin},
+                                                    {.actionName = "LOGOUT", .callback=encodeLogout},
+                                                    {.actionName = "FOLLOW", .callback=encodeFollow},
+                                                    {.actionName = "POST", .callback=encodePost},
+                                                    {.actionName = "PM", .callback=encodePM},
+                                                    {.actionName = "LOGTSTAT", .callback=encodeLogstat},
+                                                    {.actionName = "STAT", .callback=encodeStat},
+                                                    {.actionName = "BLOCK", .callback=encodeBlock}};
+
 int messageEncoderDecoder::encode(const std::string& input, char bytes[], int size) {
     int next_delimiter = input.find(' ');
     std::string action = input.substr(0, next_delimiter);
+    // Exclude the line ending and action name from the params
     next_delimiter++;
-    int length = 2;
-
-    if (action.compare("REGISTER") == 0) {
-        shortToBytes(1, bytes);
-        for (int i = 0; i < 3; i++) {
-            length += appendToBytes(&bytes[length],
-                                    input.substr(next_delimiter, input.find(' ', next_delimiter) - next_delimiter));
-            next_delimiter = input.find(' ', next_delimiter) + 1;
-            bytes[length++] = 0;
+    std::string params = input.substr(next_delimiter, input.find("\n") - next_delimiter);
+    int length = 0;
+    for (size_t i = 0; i < sizeof(handlers) / sizeof(handlers[0]); i++) {
+        if (handlers[i].actionName.compare(action) == 0) {
+            length = handlers[i].callback(params, bytes);
+            break;
         }
-    }
-    else if (action.compare("LOGIN") == 0) {
-        shortToBytes(2, bytes);
-        for (int i = 0; i < 2; i++) {
-            length += appendToBytes(&bytes[length],
-                                    input.substr(next_delimiter, input.find(' ', next_delimiter) - next_delimiter));
-            next_delimiter = input.find(' ', next_delimiter) + 1;
-            bytes[length++] = 0;
-        }
-        bytes[length++] = static_cast<char>(stoi(input.substr(next_delimiter)));
-    }
-    else if (action.compare("LOGOUT") == 0) {
-        shortToBytes(3, bytes);
-    }
-    else if (action.compare("FOLLOW") == 0) {
-        shortToBytes(4, bytes);
-        bytes[length++] = static_cast<char>(stoi(input.substr(next_delimiter)));
-        next_delimiter += 2;
-        length += appendToBytes(&bytes[length], input.substr(next_delimiter));
-        bytes[length++] = 0;
-    }
-    else if (action.compare("POST") == 0) {
-        shortToBytes(5, bytes);
-        length += appendToBytes(&bytes[length],
-                                input.substr(next_delimiter, input.find(' ', next_delimiter) - next_delimiter));
-        bytes[length++] = 0;
-    }
-    else if (action.compare("PM") == 0) {
-        shortToBytes(6, bytes);
-        for (int i = 0; i < 2; i++) {
-            length += appendToBytes(&bytes[length],
-                                    input.substr(next_delimiter, input.find(' ', next_delimiter) - next_delimiter));
-            next_delimiter = input.find(' ', next_delimiter) + 1;
-            bytes[length++] = 0;
-        }
-        std::time_t timer = std::time(0);
-        std::tm *now = std::localtime(&timer);
-        std::stringstream time_str_stream;
-        time_str_stream << std::put_time(now, "%d-%m-%Y %H-%M");
-        length += appendToBytes(&bytes[length], time_str_stream.str());
-        bytes[length++] = 0;
-    }
-    else if (action.compare("LOGSTAT") == 0) {
-        shortToBytes(7, bytes);
-    }
-    else if (action.compare("STAT") == 0) {
-        shortToBytes(8, bytes);
-        length += appendToBytes(&bytes[length],
-                                input.substr(next_delimiter, input.find(' ', next_delimiter) - next_delimiter));
-        bytes[length++] = 0;
-    }
-    else if (action.compare("BLOCK") == 0) {
-        shortToBytes(12, bytes);
-        length += appendToBytes(&bytes[length],
-                                input.substr(next_delimiter, input.find(' ', next_delimiter) - next_delimiter));
-        bytes[length++] = 0;
     }
 
     bytes[length++] = ';';
